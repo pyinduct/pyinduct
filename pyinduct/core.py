corrected another bug in control, derivatives have  not been regarded when weight transformation was trivial.

pyinduct/core.py
@@ -75,7 +75,7 @@ class BaseFraction(object):
             return self._transformation_factory(info), None
         else:
             # No Idea what to do.
-            msg = "This is {0} speaking, \n" \
+            msg = "This is {1} speaking, \n" \
                   "You requested information about how to transform to '{0}'({1}) from '{2}'({3}), \n" \
                   "furthermore the source derivative order is {4} and the target one is {4}. \n" \
                   "But this is a dumb method so implement your own hint to make things work!".format(
@@ -604,9 +604,12 @@ def get_weight_transformation(info):
     :return: handle
     """
     # trivial case
-    def identity(weights):
-        return weights
     if info.src_lbl == info.dst_lbl:
+        mat = calculate_expanded_base_transformation_matrix(info.src_base, None, info.src_order, info.dst_order, True)
+
+        def identity(weights):
+            return np.dot(mat, weights)
+
         return identity
 
     # try to get help from the destination base

tests/test_control.py
@@ -18,13 +18,14 @@ import pyinduct.shapefunctions
 
 __author__ = 'Stefan Ecklebe'
 
-if any([arg == 'discover' for arg in sys.argv]):
-    show_plots = False
-    app = None
-else:
-    # show_plots = True
-    show_plots = False
+# show_plots = True
+show_plots = False
+app = None
+
+if not any([arg == 'discover' for arg in sys.argv]):
+    import pyqtgraph as pg
     app = pg.QtGui.QApplication([])
+    show_plots = False
 
 # TODO Test for ControlLaw and LawEvaluator
 
@@ -50,7 +51,7 @@ class CollocatedTestCase(unittest.TestCase):
         self.term3 = ph.ScalarTerm(ph.Product(x_at1, exp_at1))
 
         self.weight_label = "funcs"
-        self.weights = np.hstack([1, 1, 1, 2, 2, 2])
+        self.weights = np.array([1, 1, 1, 2, 2, 2])
 
     def test_temp_term(self):
         law = ct.approximate_control_law(ct.ControlLaw([self.term1]))