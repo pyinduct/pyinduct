fd0c6db27e462a26676a26fdb49097e5f7fd96ed
marcus <marcus.riesmeier@mailbox.tu-dresden.de>
2015-11-16 09:42:56 +0100

    added some lines to avoid segfaulting

--- tests/test_control.py
+++ tests/test_control.py
@@ -416,8 +416,10 @@ class RadRobinSpatiallyVaryingCoefficientControlllerTest(unittest.TestCase):
 
         # original system parameters
         a2 = 1.5
-        a1_z = cr.Function(lambda z: 2+np.sin(2*np.pi/self.l*z), derivative_handles=[lambda z: 2*np.pi/self.l*np.cos(2*np.pi/self.l*z)])
-        a0_z = lambda z: 10+5*np.cos(5*np.pi/self.l*z)
+        a1_z = cr.Function(lambda z: 1, derivative_handles=[lambda z:0])
+        a0_z = lambda z: 3
+        # a1_z = cr.Function(lambda z: 2+np.sin(2*np.pi/self.l*z), derivative_handles=[lambda z: 2*np.pi/self.l*np.cos(2*np.pi/self.l*z)])
+        # a0_z = lambda z: 10+5*np.cos(5*np.pi/self.l*z)
         alpha = -2
         beta = -3
         # a2 = 1.5; a1_z = cr.Function(lambda z: 1+z, derivative_handles=[lambda z: 1]); a0_z = lambda z: 2-z; alpha = -2; beta = -3
@@ -663,93 +665,3 @@ class RadRobinInDomainBacksteppingControllerTest(unittest.TestCase):
             win3 = vis.AnimatedPlot([eval_d], title="fem")
             win4 = vis.SurfacePlot(eval_d)
             app.exec_()
-
-
-class SimulationError(unittest.TestCase):
-    """
-    """
-    def setUp(self):
-        pass
-
-    def test_simulate_state_space_error(self):
-        pass
-        # original system parameters
-        a2 = 1.5; a1 = 2.5; a0 = 28; alpha = -2; beta = -3
-        param = [a2, a1, a0, alpha, beta]
-        adjoint_param = ef.get_adjoint_rad_evp_param(param)
-
-        # target system parameters (controller parameters)
-        a1_t = -5; a0_t = -25; alpha_t = 3; beta_t = 2
-        # a1_t = a1; a0_t = a0; alpha_t = alpha; beta_t = beta
-        param_t = [a2, a1_t, a0_t, alpha_t, beta_t]
-
-        # original intermediate ("_i") and traget intermediate ("_ti") system parameters
-        _, _, a0_i, alpha_i, beta_i = ef.transform2intermediate(param)
-        param_i = a2, 0, a0_i, alpha_i, beta_i
-        _, _, a0_ti, alpha_ti, beta_ti = ef.transform2intermediate(param_t)
-        param_ti = a2, 0, a0_ti, alpha_ti, beta_ti
-
-        # system/simulation parameters
-        actuation = 'robin'
-        boundary_condition = 'robin'
-        l = 1.; spatial_domain = (0, l); spatial_disc = 10
-        T = 1; temporal_domain = (0, T); temporal_disc = 1e2
-        n = 10
-
-        # create (not normalized) eigenfunctions
-        eig_freq, eig_val = ef.compute_rad_robin_eigenfrequencies(param, l, n)
-        init_eig_funcs = np.array([ef.SecondOrderRobinEigenfunction(om, param, spatial_domain) for om in eig_freq])
-        init_adjoint_eig_funcs = np.array([ef.SecondOrderRobinEigenfunction(om, adjoint_param, spatial_domain) for om in eig_freq])
-
-        # normalize eigenfunctions and adjoint eigenfunctions
-        adjoint_and_eig_funcs = [cr.normalize_function(init_eig_funcs[i], init_adjoint_eig_funcs[i]) for i in range(n)]
-        eig_funcs = np.array([f_tuple[0] for f_tuple in adjoint_and_eig_funcs])
-        adjoint_eig_funcs = np.array([f_tuple[1] for f_tuple in adjoint_and_eig_funcs])
-
-        # eigenfunctions from target system ("_t")
-        eig_freq_t = np.sqrt(-a1_t**2/4/a2**2 + (a0_t - eig_val)/a2)
-        eig_funcs_t = np.array([ef.SecondOrderRobinEigenfunction(eig_freq_t[i], param_t, spatial_domain).scale(eig_funcs[i](0)) for i in range(n)])
-
-        # register eigenfunctions
-        register_functions("eig_funcs", eig_funcs, overwrite=True)
-        register_functions("adjoint_eig_funcs", adjoint_eig_funcs, overwrite=True)
-        register_functions("eig_funcs_t", eig_funcs_t, overwrite=True)
-
-        # derive initial field variable x(z,0) and weights
-        start_state = cr.Function(lambda z: 0., domain=(0, l))
-        initial_weights = cr.project_on_base(start_state, adjoint_eig_funcs)
-
-        # init trajectory
-        traj = tr.RadTrajectory(l, T, param_t, boundary_condition, actuation)
-
-        # alternetiv controller initialization (shorter but less generic)
-        x_at_l = ph.FieldVariable("eig_funcs", location=l)
-        xd_at_l = ph.SpatialDerivedFieldVariable("eig_funcs", 1, location=l)
-        x_t_at_l = ph.FieldVariable("eig_funcs_t", weight_label="eig_funcs", location=l)
-        xd_t_at_l = ph.SpatialDerivedFieldVariable("eig_funcs_t", 1, weight_label="eig_funcs", location=l)
-        combined_transform = lambda z: np.exp((a1_t-a1)/2/a2*z)
-        int_kernel_zz = lambda z: alpha_ti - alpha_i + (a0_i-a0_ti)/2/a2*z
-        control_law = ct.Controller(
-            ct.ControlLaw([ph.ScalarTerm(x_at_l, (beta_i-beta_ti-int_kernel_zz(l))),
-                           ph.ScalarTerm(x_t_at_l, -beta_ti*combined_transform(l)),
-                           ph.ScalarTerm(x_at_l, beta_ti),
-                           ph.ScalarTerm(xd_t_at_l, -combined_transform(l)),
-                           ph.ScalarTerm(x_t_at_l, -a1_t/2/a2*combined_transform(l)),
-                           ph.ScalarTerm(xd_at_l, l),
-                           ph.ScalarTerm(x_at_l, a1/2/a2+int_kernel_zz(l))
-                           ]))
-        # input with feedback
-        controller = sim.Mixer([traj, control_law])
-
-        # determine (A,B) with modal-transfomation
-        A = np.diag(np.real(eig_val))
-        B = a2*np.array([adjoint_eig_funcs[i](l) for i in xrange(len(eig_freq))])
-        ss_modal = sim.StateSpace("eig_funcs", A, B)
-
-        # simulate
-        # t, q = sim.simulate_state_space(ss_modal, control_law, initial_weights, temporal_domain, time_step=T/temporal_disc)
-
-    def test_equivalent_problem(self):
-        a = np.arange(1,3)
-        b = np.array([1])
-        # np.dot(a, b)
--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -6,33 +6,35 @@ from cPickle import loads
 
 from pyinduct.visualization import SlicePlot, LinePLot3d
 import pyqtgraph as pg
+import sys
 
 __author__ = 'stefan'
 
-# show_plots = True
-show_plots = False
+if any([arg == 'discover' for arg in sys.argv]):
+    show_plots = False
+else:
+    app = pg.QtGui.QApplication([])
+    show_plots = True
 
 
 class PlotTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = pg.QtGui.QApplication([])
-
-        try:
-            with open(os.sep.join(["resources", "test_data.res"])) as f:
-                self.test_data = loads(f.read())
-        except:
-            raise ValueError("run 'test_simulation' first!")
+        if show_plots:
+            try:
+                with open(os.sep.join(["resources", "test_data.res"])) as f:
+                    self.test_data = loads(f.read())
+            except:
+                raise ValueError("run 'test_simulation' first!")
 
     def test_slice_plot(self):
-        pt = SlicePlot(self.test_data[0])
-        self.app.exec_()
+        if show_plots:
+            pt = SlicePlot(self.test_data[0])
+            app.exec_()
 
     def test_3d_line_plot(self):
-        pt = LinePLot3d(self.test_data)
-        self.app.exec_()
-
-    def tearDown(self):
-        del self.app
+        if show_plots:
+            pt = LinePLot3d(self.test_data)
+            app.exec_()
 
 
 
