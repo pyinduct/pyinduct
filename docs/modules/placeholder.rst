483f444d83db8f18b15f7aa5e2f4eefaab42d8a2
Marcus Riesmeier <marcus.riesmeier@umit.at>
2016-07-26 10:19:47 +0200

    Add fieldvariable and testfunction factory methods
    * add factory methods for a shorter respectivly clearer implementation
    * first application: see system and controller implementation
      in examples/swm_observer.py

--- docs/modules/placeholder.rst
+++ docs/modules/placeholder.rst
@@ -6,4 +6,14 @@ Placeholder
     :members:
     :undoc-members:
     :show-inheritance:
+    :exclude-members: FieldVariable, TestFunction
 
+    .. autoclass:: FieldVariable
+        :members: __call__, derive_spat, derive_temp
+        :undoc-members:
+        :show-inheritance:
+
+    .. autoclass:: TestFunction
+        :members: __call__, derive
+        :undoc-members:
+        :show-inheritance:
--- examples/swm_observer.py
+++ examples/swm_observer.py
@@ -11,67 +11,62 @@ import numpy as np
 from pyqtgraph.Qt import QtGui
 
 
-def build_weak_form(label, spatial_interval, u, params):
-    p1 = ph.Product(ph.TemporalDerivedFieldVariable(label, order=2),
-                    ph.TestFunction(label))
-    t1 = ph.IntegralTerm(p1, limits=spatial_interval)
-    p2 = ph.Product(ph.SpatialDerivedFieldVariable(label, 1),
-                    ph.TestFunction(label, order=1))
-    t2 = ph.IntegralTerm(p2, limits=spatial_interval)
-    p3 = ph.Product(ph.TemporalDerivedFieldVariable(label, order=2, location=0),
-                    ph.TestFunction(label, location=0))
-    t3 = ph.ScalarTerm(p3, scale=params.m)
-    p4 = ph.Product(ph.Input(u), ph.TestFunction(label, location=1))
-    t4 = ph.ScalarTerm(p4, scale=-1)
+def build_weak_form(approx_label, spatial_interval, u, params):
+    x = ph.FieldVariable(approx_label)
+    psi = ph.TestFunction(approx_label)
+    term1 = ph.IntegralTerm(ph.Product(x.derive_temp(2), psi), limits=spatial_interval)
+    term2 = ph.IntegralTerm(ph.Product(x.derive_spat(1), psi.derive(1)), limits=spatial_interval)
+    term3 = ph.ScalarTerm(ph.Product(x(0).derive_temp(2), psi(0)), scale=params.m)
+    term4 = ph.ScalarTerm(ph.Product(ph.Input(u), psi(1)), scale=-1)
 
-    return sim.WeakFormulation([t1, t2, t3, t4], name="swm_fem")
+    return sim.WeakFormulation([term1, term2, term3, term4], name="swm_system")
 
 
-def build_control_law(sys_label, params):
-    dz_x1 = ph.SpatialDerivedFieldVariable(sys_label, order=1)
-    x2 = ph.TemporalDerivedFieldVariable(sys_label, order=1)
-    xi1 = ph.FieldVariable(sys_label, location=0)
-    xi2 = ph.TemporalDerivedFieldVariable(sys_label, order=1, location=0)
-
-    x2_at1 = ph.TemporalDerivedFieldVariable(sys_label, order=1, location=1)
+def build_control_law(approx_label, params):
+    x = ph.FieldVariable(approx_label)
+    dz_x1 = x.derive_spat(1)
+    x2 = x.derive_temp(1)
+    xi1 = x(0)
+    xi2 = x(0).derive_temp(1)
 
     scalar_scale_funcs = [cr.Function(lambda theta: params.m * (1 - np.exp(-theta / params.m))),
                           cr.Function(lambda theta: params.m * (-1 + np.exp(theta / params.m))),
                           cr.Function(lambda theta: np.exp(-theta / params.m)),
                           cr.Function(lambda theta: np.exp(theta / params.m))]
 
-    register_base("int_scale_func1", cr.Function(lambda tau: 1 - np.exp(-(1 - tau) / params.m)))
-    register_base("int_scale_func2", cr.Function(lambda tau: -1 + np.exp((-1 + tau) / params.m)))
-    register_base("int_scale_func3", cr.Function(lambda tau: np.exp(-(1 - tau) / params.m) / params.m))
-    register_base("int_scale_func4", cr.Function(lambda tau: np.exp((-1 + tau) / params.m) / params.m))
+    register_base("int_scale1", cr.Function(lambda tau: 1 - np.exp(-(1 - tau) / params.m)))
+    register_base("int_scale2", cr.Function(lambda tau: -1 + np.exp((-1 + tau) / params.m)))
+    register_base("int_scale3", cr.Function(lambda tau: np.exp(-(1 - tau) / params.m) / params.m))
+    register_base("int_scale4", cr.Function(lambda tau: np.exp((-1 + tau) / params.m) / params.m))
 
     limits = (0, 1)
     y_bar_plus1 = [ph.ScalarTerm(xi1),
                    ph.ScalarTerm(xi2, scale=scalar_scale_funcs[0](1)),
-                   ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale_func1"), dz_x1), limits=limits),
-                   ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale_func1"), x2), limits=limits)]
+                   ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale1"), dz_x1), limits=limits),
+                   ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale1"), x2), limits=limits)]
     y_bar_minus1 = [ph.ScalarTerm(xi1),
                     ph.ScalarTerm(xi2, scale=scalar_scale_funcs[1](-1)),
-                    ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale_func2"), dz_x1), limits=limits, scale=-1),
-                    ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale_func2"), x2), limits=limits)]
+                    ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale2"), dz_x1), limits=limits, scale=-1),
+                    ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale2"), x2), limits=limits)]
     dz_y_bar_plus1 = [ph.ScalarTerm(xi2, scale=scalar_scale_funcs[2](1)),
-                      ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale_func3"), dz_x1), limits=limits),
-                      ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale_func3"), x2), limits=limits)]
+                      ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale3"), dz_x1), limits=limits),
+                      ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale3"), x2), limits=limits)]
     dz_y_bar_minus1 = [ph.ScalarTerm(xi2, scale=scalar_scale_funcs[3](-1)),
-                       ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale_func4"), dz_x1), limits=limits, scale=-1),
-                       ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale_func4"), x2), limits=limits)]
+                       ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale4"), dz_x1), limits=limits, scale=-1),
+                       ph.IntegralTerm(ph.Product(ph.ScalarFunction("int_scale4"), x2), limits=limits)]
 
-    return ct.ControlLaw(
-        [ph.ScalarTerm(x2_at1, scale=-(1 - params.alpha) / (1 + params.alpha))] +
-        ut.scale_equation_term_list(dz_y_bar_plus1,
-                                    factor=(1 - params.m * params.k1) / (1 + params.alpha)) +
-        ut.scale_equation_term_list(dz_y_bar_minus1,
-                                    factor=-params.alpha * (1 + params.m * params.k1) / (1 + params.alpha)) +
-        ut.scale_equation_term_list(y_bar_plus1,
-                                    factor=-params.m * params.k0 / (1 + params.alpha)) +
-        ut.scale_equation_term_list(y_bar_minus1,
-                                    factor=-params.alpha * params.m * params.k0 / (1 + params.alpha))
-    )
+    return ct.ControlLaw(ut.scale_equation_term_list(
+        [ph.ScalarTerm(x2(1), scale=-(1 - params.alpha))] +
+        ut.scale_equation_term_list(dz_y_bar_plus1, factor=(1 - params.m * params.k1)) +
+        ut.scale_equation_term_list(dz_y_bar_minus1, factor=-params.alpha * (1 + params.m * params.k1)) +
+        ut.scale_equation_term_list(y_bar_plus1, factor=-params.m * params.k0) +
+        ut.scale_equation_term_list(y_bar_minus1, factor=-params.alpha * params.m * params.k0),
+        factor=(1 + params.alpha) ** -1
+    ))
+
+
+def build_observer():
+    pass
 
 
 class SecondOrderFeedForward(sim.SimulationInput):
@@ -87,10 +82,6 @@ class SecondOrderFeedForward(sim.SimulationInput):
             + self._params.k1 * (y_p[1] + self._params.alpha * y_m[1]) \
             + y_p[2] + self._params.alpha * y_m[2]
         return dict(output=self._params.m / (1 + self._params.alpha) * f)
-
-
-def build_observer():
-    pass
 
 
 class Parameters:
@@ -130,10 +121,13 @@ ic = np.array([
     cr.Function(lambda z: dt_x_zt(z, 0)),
 ])
 
-nodes, ini_funcs = sh.cure_interval(sh.LagrangeFirstOrder,
-                                    spat_domain.bounds,
-                                    node_count=params.node_count)
-register_base("sim", ini_funcs)
+# initial function
+sys_nodes, sys_funcs = sh.cure_interval(sh.LagrangeFirstOrder, spat_domain.bounds, node_count=10)
+ctrl_nodes, ctrl_funcs = sh.cure_interval(sh.LagrangeFirstOrder, spat_domain.bounds, node_count=20)
+obs_nodes, obs_funcs = sh.cure_interval(sh.LagrangeSecondOrder, spat_domain.bounds, node_count=7)
+register_base("sim", sys_funcs)
+register_base("ctrl", ctrl_funcs)
+register_base("obs", obs_funcs)
 
 # system input
 if 1:
@@ -141,7 +135,7 @@ if 1:
     smooth_transition = tr.SmoothTransition((0, 1), (1, 3), method="poly", differential_order=2)
     closed_loop_traj = SecondOrderFeedForward(smooth_transition, params)
     # controller
-    ctrl = ct.Controller(build_control_law("sim", params))
+    ctrl = ct.Controller(build_control_law("ctrl", params))
     u = sim.SimulationInputSum([closed_loop_traj, ctrl])
 else:
     # trajectory for the original input (open_loop_traj)
