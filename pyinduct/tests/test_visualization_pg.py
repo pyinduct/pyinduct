3eb132f69e1890558e9caf1fc390f10b18bf3f71
Jens Wurm <jens.wurm@umit.at>
2017-11-23 16:30:00 +0100

    Update test order, animated surface must first, otherwise "unexpected exception"

--- pyinduct/tests/test_visualization_pg.py
+++ pyinduct/tests/test_visualization_pg.py
@@ -24,41 +24,47 @@ class PlotTestCase(unittest.TestCase):
             self.test_data[0].output_data[0:lim, 1],
             name="2d set")
 
-    def test_2d_plot(self):
+    def test_plots(self):
+        self._test_animated_surface_plot()
+        self._test_surface_plot()
+        self._test_2d_plot()
+        self._test_2d_animated_plot()
+
+    def _test_2d_plot(self):
         pt = vis.Pg2DPlot(data=self.test_2d_data)
         if show_plots:
             pi.show(show_mpl=False)
 
-    def test_2d_animated_plot(self):
+    def _test_2d_animated_plot(self):
         pt = vis.Pg2DPlot(data=self.test_data, animationAxis=0)
         if show_plots:
             pi.show(show_mpl=False)
 
-    # def test_surface_plot(self):
-    #     pt = vis.PgSurfacePlot(data=self.test_data)
-    #     if show_plots:
-    #         pi.show(show_mpl=False)
+    def _test_surface_plot(self):
+        pt = vis.PgSurfacePlot(data=self.test_data)
+        if show_plots:
+            pi.show(show_mpl=False)
 
-    # def test_animated_surface_plot(self):
-    #
-    #     def data_func(x, y, t):
-    #         d = (x**2 + y**2)/5
-    #         return np.exp(-d/10) * np.sin(d - t)
-    #
-    #     x_arr = np.linspace(-7, 7, 100)
-    #     y_arr = np.linspace(-5, 5, 200)
-    #     t_arr = np.linspace(0, 10*np.pi, 300)
-    #
-    #     xx, yy, tt = np.meshgrid(y_arr, x_arr, t_arr)
-    #     data = data_func(xx, yy, tt)
-    #
-    #     data_set_0 = pi.EvalData([t_arr, x_arr, y_arr],
-    #                              np.rollaxis(data, 2))
-    #
-    #     # animation axis has to be provided for 3d data
-    #     self.assertRaises(ValueError, pi.PgSurfacePlot, data_set_0)
-    #
-    #     pt = vis.PgSurfacePlot(data=data_set_0, animationAxis=0)
-    #
-    #     if show_plots:
-    #         pi.show(show_mpl=False)
+    def _test_animated_surface_plot(self):
+
+        def data_func(x, y, t):
+            d = (x**2 + y**2)/5
+            return np.exp(-d/10) * np.sin(d - t)
+
+        x_arr = np.linspace(-7, 7, 100)
+        y_arr = np.linspace(-5, 5, 200)
+        t_arr = np.linspace(0, 10*np.pi, 300)
+
+        xx, yy, tt = np.meshgrid(y_arr, x_arr, t_arr)
+        data = data_func(xx, yy, tt)
+
+        data_set_0 = pi.EvalData([t_arr, x_arr, y_arr],
+                                 np.rollaxis(data, 2))
+
+        # animation axis has to be provided for 3d data
+        self.assertRaises(ValueError, pi.PgSurfacePlot, data_set_0)
+
+        pt = vis.PgSurfacePlot(data=data_set_0, animationAxis=0)
+
+        if show_plots:
+            pi.show(show_mpl=False)
