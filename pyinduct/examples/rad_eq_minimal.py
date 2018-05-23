Bugfix input derivatives (#88)
* Added support and tests for vectorial inputs with temporal derivatives

* Added support for derived and vectorial inputs in StateSpace class

* Added example to demonstrate usage of multiple inputs

* Corrected errors in tests and added type checks

* Fixed a remaining error

pyinduct/examples/rad_eq_minimal.py
@@ -77,7 +77,7 @@ if __name__ == "__main__" or test_examples:
     A = np.diag(np.real_if_close(eig_values))
     B = -a2 * np.array([eig_funcs[i].derive()(l) for i in range(n)])
     B = np.reshape(B, (B.size, 1))
-    ss = pi.StateSpace(A, B, base_lbl="eig_funcs", input_handle=control_law)
+    ss = pi.StateSpace(A, B, base_lbl="eig_funcs", input_handles=control_law)
 
     # evaluate desired output data
     z_d = np.linspace(0, l, len(spatial_domain))

pyinduct/simulation.py
@@ -5,7 +5,7 @@ and functions for postprocessing of simulation data.
 
 import warnings
 from abc import ABCMeta, abstractmethod
-from collections import OrderedDict
+from collections import OrderedDict, Callable
 from copy import copy
 from itertools import chain
 
@@ -195,13 +195,14 @@ class StateSpace(object):
             for the corresponding powers of :math:`\boldsymbol{x}`
         b_matrices (dict): Cascaded dictionary for the input matrices :math:`\boldsymbol{B}_{j, k}` in the sequence:
             temporal derivative order, exponent .
-        input_handle:  function handle, returning the system input :math:`u(t)`
+        input_handles (np.ndarray):  (Array of) callables for the the system
+            inputs :math:`u(t)`.
         c_matrix: :math:`\boldsymbol{C}`
         d_matrix: :math:`\boldsymbol{D}`
     """
 
     def __init__(self, a_matrices, b_matrices, base_lbl=None,
-                 input_handle=None, c_matrix=None, d_matrix=None):
+                 input_handles=None, c_matrix=None, d_matrix=None):
         self.C = c_matrix
         self.D = d_matrix
         self.base_lbl = base_lbl
@@ -231,12 +232,10 @@ class StateSpace(object):
         if self.D is None:
             self.D = np.zeros((self.C.shape[0], np.atleast_2d(self.B[0][available_power]).T.shape[1]))
 
-        self.input = input_handle
-        if self.input is None:
+        if input_handles is None:
             self.input = EmptyInput(self.B[0][available_power].shape[1])
-
-        if not callable(self.input):
-            raise TypeError("input must be callable!")
+        else:
+            self.input = sanitize_input(input_handles, SimulationInput)
 
     # TODO export cython code?
     def rhs(self, _t, _q):
@@ -250,17 +249,19 @@ class StateSpace(object):
         Returns:
             (array): :math:`\boldsymbol{\dot{x}}(t)`
         """
-        q_t = self.A[0]
-        for p, a_mat in self.A.items():
-            q_t = q_t + a_mat @ np.power(_q, p)
+        state_part = self.A[0]
+        for power, a_mat in self.A.items():
+            state_part = state_part + a_mat @ np.power(_q, power)
 
-        # TODO make compliant with definition of temporal derived input
-        u = self.input(time=_t, weights=_q, weight_lbl=self.base_lbl)
-        for o, p_mats in self.B.items():
-            for p, b_mat in p_mats.items():
-                # q_t = q_t + (b_mat @ np.power(u, p)).flatten()
-                q_t = q_t + b_mat @ np.power(u, p)
+        input_part = np.zeros_like(state_part)
+        inputs = np.atleast_2d([u(time=_t, weights=_q, weight_lbl=self.base_lbl)
+                                for u in self.input])
+        for der_order, power_dict in self.B.items():
+            for power, b_mat in power_dict.items():
+                for idx, col in enumerate(b_mat.T):
+                    input_part = input_part + col * inputs[idx, der_order]
 
+        q_t = state_part + input_part
         return q_t
 
 
@@ -453,7 +454,7 @@ class CanonicalForm(object):
         self.matrices = {}
         # self._max_idx = dict(E=0, f=0, G=0)
         self._weights = None
-        self._input_function = None
+        self._input_functions = None
         self._finalized = False
         self.powers = None
         self.max_power = None
@@ -476,15 +477,26 @@ class CanonicalForm(object):
     #                 self._matrices[name][der][p] += pow
 
     @property
-    def input_function(self):
-        return self._input_function
+    def input_functions(self):
+        return self._input_functions
 
-    @input_function.setter
-    def input_function(self, func):
-        if self._input_function is None:
-            self._input_function = func
-        if self._input_function != func:
-            raise ValueError("already defined input is overridden!")
+    def set_input_function(self, func, index=0):
+        if not isinstance(func, SimulationInput):
+            raise TypeError("Inputs must be of type `SimulationInput`.")
+
+        if self._input_functions is None:
+            self._input_functions = np.atleast_1d(func)
+
+        # check whether the dimensions must be extended
+        if index >= self.input_functions.shape[0]:
+            old_len = self._input_functions.shape[0]
+            new_input_functions = np.empty(index + 1, dtype=object)
+            new_input_functions[:old_len] = self._input_functions
+            self._input_functions = new_input_functions
+            self._input_functions[index] = func
+        else:
+            if self._input_functions[index] != func:
+                raise ValueError("already defined input is overridden!")
 
     # @property
     # def weights(self):
@@ -688,7 +700,7 @@ class CanonicalForm(object):
         a_matrices.update({0: f_mat})
 
         ss = StateSpace(a_matrices, b_matrices,
-                        input_handle=self.input_function)
+                        input_handles=self.input_functions)
         return ss
 
     def _build_feedback(self, entry, power, product_mat):
@@ -835,15 +847,14 @@ class CanonicalEquation(object):
         return {label: val.get_terms() for label, val in self.dynamic_forms.items()}
 
     @property
-    def input_function(self):
+    def input_functions(self):
         """
-        The input handle for the equation.
+        The input handles for the equation.
         """
-        return self._static_form.input_function
+        return self._static_form.input_functions
 
-    @input_function.setter
-    def input_function(self, func):
-        self._static_form.input_function = func
+    def set_input_function(self, func, index=0):
+        self._static_form.set_input_function(func, index)
 
 
 def create_state_space(canonical_equations):
@@ -920,9 +931,9 @@ def create_state_space(canonical_equations):
 
         # update input handles
         if state_space_props.input is None:
-            state_space_props.input = eq.input_function
+            state_space_props.input = eq.input_functions
         else:
-            if eq.input_function is not None and state_space_props.input != eq.input_function:
+            if eq.input_functions is not None and state_space_props.input != eq.input_functions:
                 raise ValueError("Only one input object allowed.")
 
     # build new basis by concatenating the dominant bases of every equation
@@ -986,7 +997,7 @@ def create_state_space(canonical_equations):
             b_matrices.update({order: b_order_mats})
 
     dom_ss = StateSpace(a_matrices, b_matrices, base_lbl=new_name,
-                        input_handle=state_space_props.input)
+                        input_handles=state_space_props.input)
     return dom_ss
 
 
@@ -1138,7 +1149,7 @@ def parse_weak_formulation(weak_form, finalize=False):
 
                 ce.add_to(weight_label=None, term=term_info,
                           val=result * term.scale, column=input_index)
-                ce.input_function = input_func
+                ce.set_input_function(input_func, input_index)
                 continue
 
         # pure scalar terms, sort into corresponding matrices
@@ -1155,11 +1166,10 @@ def parse_weak_formulation(weak_form, finalize=False):
                 input_index = input_var.data["index"]
                 input_exp = input_var.data["exponent"]
                 input_order = input_var.order[0]
-                term_info = dict(name="G", order=input_order, exponent=input_exp)
 
-                if input_order > 0:
-                    # derivative handels of the callablewould be needed here
-                    raise NotImplementedError
+                term_info = dict(name="G",
+                                 order=input_order,
+                                 exponent=input_exp)
 
                 if target["name"] == "E":
                     # this would mean that the input term should appear in a
@@ -1169,7 +1179,7 @@ def parse_weak_formulation(weak_form, finalize=False):
 
                 ce.add_to(weight_label=None, term=term_info,
                           val=result * term.scale, column=input_index)
-                ce.input_function = input_func
+                ce.set_input_function(input_func, input_index)
                 continue
 
             ce.add_to(weight_label=target_form, term=target,
@@ -1251,11 +1261,6 @@ def simulate_state_space(state_space, initial_state, temp_domain, settings=None)
     """
     if not isinstance(state_space, StateSpace):
         raise TypeError
-
-    input_handle = state_space.input
-
-    if not isinstance(input_handle, SimulationInput):
-        raise TypeError("only simulation.SimulationInput supported.")
 
     q = [initial_state]
     t = [temp_domain[0]]

pyinduct/tests/test_simulation.py
@@ -37,10 +37,10 @@ class CorrectInput(sim.SimulationInput):
     """
     a diligent input
     """
-    def __init__(self, limits):
+    def __init__(self, output, limits=(0, 1), der_order=0):
         super().__init__(self)
-        self.t_min = limits[0]
-        self.t_max = limits[1]
+        self.out = np.ones(der_order + 1) * output
+        self.t_min, self.t_max = limits
 
     def _calc_output(self, **kwargs):
         if "time" not in kwargs:
@@ -49,7 +49,7 @@ class CorrectInput(sim.SimulationInput):
             raise ValueError("mandatory key not found!")
         if "weight_lbl" not in kwargs:
             raise ValueError("mandatory key not found!")
-        return dict(output=0)
+        return dict(output=self.out)
 
 
 class AlternatingInput(sim.SimulationInput):
@@ -86,9 +86,9 @@ class SimulationInputTest(unittest.TestCase):
     def test_call_arguments(self):
         a = np.eye(2, 2)
         b = np.array([[0], [1]])
-        u = CorrectInput(limits=(0, 1))
+        u = CorrectInput(output=1, limits=(0, 1))
         ic = np.zeros((2, 1))
-        ss = sim.StateSpace({1: a}, {0: {1: b}}, input_handle=u)
+        ss = sim.StateSpace({1: a}, {0: {1: b}}, input_handles=u)
 
         # if caller provides correct kwargs no exception should be raised
         res = sim.simulate_state_space(ss, ic, pi.Domain((0, 1), num=10))
@@ -98,7 +98,7 @@ class SimulationInputTest(unittest.TestCase):
         b = np.array([[0], [1]])
         u = MonotonousInput()
         ic = np.zeros((2, 1))
-        ss = sim.StateSpace(a, b, input_handle=u)
+        ss = sim.StateSpace(a, b, input_handles=u)
 
         # run simulation to fill the internal storage
         domain = pi.Domain((0, 10), step=.1)
@@ -151,16 +151,21 @@ class CanonicalFormTest(unittest.TestCase):
         self.assertTrue(np.array_equal(self.cf.matrices["E"][0][1], 6 * a))
 
         b = np.eye(10)
-        self.assertRaises(ValueError, self.cf.add_to, dict(name="E", order=0, exponent=1), b)
+        self.assertRaises(ValueError,
+                          self.cf.add_to,
+                          dict(name="E", order=0, exponent=1), b)
         self.cf.add_to(dict(name="E", order=2, exponent=1), b)
         self.assertTrue(np.array_equal(self.cf.matrices["E"][2][1], b))
 
         f = np.atleast_2d(np.array(range(5))).T
-        self.assertRaises(ValueError, self.cf.add_to, dict(name="E", order=0, exponent=1), f)
+        self.assertRaises(ValueError,
+                          self.cf.add_to,
+                          dict(name="E", order=0, exponent=1), f)
         self.cf.add_to(dict(name="f"), f)
         self.assertTrue(np.array_equal(self.cf.matrices["f"], f))
 
-        # try to add something with derivative or exponent to f: value should end up in f
+        # try to add something with derivative or exponent to f: value should
+        #  end up in f
         self.cf.add_to(dict(name="f"), f)
         self.assertTrue(np.array_equal(self.cf.matrices["f"], 2 * f))
 
@@ -171,11 +176,22 @@ class CanonicalFormTest(unittest.TestCase):
 
         # here G01 as to be expanded
         self.cf.add_to(dict(name="G", order=0, exponent=1), c, column=1)
-        self.assertTrue(np.array_equal(self.cf.matrices["G"][0][1], np.hstack((c, c))))
+        self.assertTrue(np.array_equal(self.cf.matrices["G"][0][1],
+                                       np.hstack((c, c))))
 
         # here G01 as to be expanded again
         self.cf.add_to(dict(name="G", order=0, exponent=1), c, column=3)
-        self.assertTrue(np.array_equal(self.cf.matrices["G"][0][1], np.hstack((c, c, np.zeros_like(c), c))))
+        self.assertTrue(np.array_equal(self.cf.matrices["G"][0][1],
+                                       np.hstack((c, c, np.zeros_like(c), c))))
+
+        # input derivatives can occur
+        self.cf.add_to(dict(name="G", order=1, exponent=1), c, column=0)
+        self.assertTrue(np.array_equal(self.cf.matrices["G"][1][1], c))
+
+        # expansion should still work
+        self.cf.add_to(dict(name="G", order=1, exponent=1), c, column=1)
+        self.assertTrue(np.array_equal(self.cf.matrices["G"][1][1],
+                                       np.hstack((c, c))))
 
 
 class ParseTest(unittest.TestCase):
@@ -183,10 +199,26 @@ class ParseTest(unittest.TestCase):
         # scalars
         self.scalars = pi.Scalars(np.vstack(list(range(3))))
 
+        # callbacks
+        self.u = pi.ConstantTrajectory(7)
+        self.u1 = CorrectInput(output=1)
+        self.u2 = CorrectInput(output=2)
+        self.u_dt = CorrectInput(output=1, der_order=1)
+        self.u1_dt = CorrectInput(output=1, der_order=1)
+        self.u2_dt = CorrectInput(output=2, der_order=1)
+
         # inputs
-        self.u = np.sin
         self.input = pi.Input(self.u)
+
         self.input_squared = pi.Input(self.u, exponent=2)
+
+        self.vec_input_1 = pi.Input(self.u1, index=0)
+        self.vec_input_2 = pi.Input(self.u2, index=1)
+
+        self.input_dt = pi.Input(self.u_dt, order=1)
+
+        self.vec_input_dt_1 = pi.Input(self.u1_dt, index=0, order=1)
+        self.vec_input_dt_2 = pi.Input(self.u2_dt, index=1, order=1)
 
         # scale function
         def heavyside(z):
@@ -276,6 +308,20 @@ class ParseTest(unittest.TestCase):
             pi.Product(pi.Product(self.scalar_func, self.test_funcs),
                        self.input),
             limits=(.5, 1))
+
+        self.input_term_dt = pi.IntegralTerm(pi.Product(self.test_funcs,
+                                                        self.input_dt),
+                                             limits=(0, 1))
+
+        self.input_term_vectorial1 = pi.ScalarTerm(
+            pi.Product(self.test_funcs_at0, self.vec_input_1))
+        self.input_term_vectorial2 = pi.ScalarTerm(
+            pi.Product(self.test_funcs_at1, self.vec_input_2))
+
+        self.input_term_vectorial_dt1 = pi.ScalarTerm(
+            pi.Product(self.test_funcs_at0, self.vec_input_dt_1))
+        self.input_term_vectorial_dt2 = pi.ScalarTerm(
+            pi.Product(self.test_funcs_at1, self.vec_input_dt_2))
 
         # pure test function terms
         self.func_term = pi.ScalarTerm(self.test_funcs_at1)
@@ -378,9 +424,9 @@ class ParseTest(unittest.TestCase):
         terms = sim.parse_weak_formulation(
             sim.WeakFormulation(self.input_term3, name="test"),
             finalize=False).get_static_terms()
+        self.assertFalse(np.iscomplexobj(terms["G"][0][1]))
         np.testing.assert_array_almost_equal(terms["G"][0][1],
                                              np.array([[.25], [.5], [.25]]))
-        self.assertFalse(np.iscomplexobj(terms["G"][0][1]))
 
         terms = sim.parse_weak_formulation(
             sim.WeakFormulation(self.input_term3_swapped, name="test"),
@@ -409,6 +455,37 @@ class ParseTest(unittest.TestCase):
         self.assertFalse(np.iscomplexobj(terms["G"][0][1]))
         np.testing.assert_array_almost_equal(terms_sh["G"][0][1],
                                              np.array([[.0], [.25], [.25]]))
+
+        # vectorial inputs
+        terms = sim.parse_weak_formulation(sim.WeakFormulation(
+            [self.input_term_vectorial1, self.input_term_vectorial2],
+            name="test"),
+            finalize=False).get_static_terms()
+        self.assertFalse(np.iscomplexobj(terms["G"][0][1]))
+        np.testing.assert_array_almost_equal(terms["G"][0][1],
+                                             np.array([[1, 0],
+                                                       [0, 0],
+                                                       [0, 1]]))
+
+        # time derivatives of inputs
+        terms = sim.parse_weak_formulation(sim.WeakFormulation(
+            self.input_term_dt,
+            name="test"),
+            finalize=False).get_static_terms()
+        self.assertFalse(np.iscomplexobj(terms["G"][1][1]))
+        np.testing.assert_array_almost_equal(terms["G"][1][1],
+                                             np.array([[.25], [.5], [.25]]))
+
+        # time derivative of vectorial inputs
+        terms = sim.parse_weak_formulation(sim.WeakFormulation(
+            [self.input_term_vectorial_dt1, self.input_term_vectorial_dt2],
+            name="test"),
+            finalize=False).get_static_terms()
+        self.assertFalse(np.iscomplexobj(terms["G"][1][1]))
+        np.testing.assert_array_almost_equal(terms["G"][1][1],
+                                             np.array([[1, 0],
+                                                       [0, 0],
+                                                       [0, 1]]))
 
     def test_TestFunction_term(self):
         terms = sim.parse_weak_formulation(
@@ -703,7 +780,7 @@ class StateSpaceTests(unittest.TestCase):
         pi.register_base("swm_base", lag_base)
 
         # input
-        self.u = CorrectInput(limits=(0, 10))
+        self.u = CorrectInput(output=5, limits=(0, 10))
         # self.u = CorrectInput(limits=self.time_domain.bounds)
 
         field_var = pi.FieldVariable("swm_base")
@@ -740,7 +817,7 @@ class StateSpaceTests(unittest.TestCase):
         np.testing.assert_array_almost_equal(
             ss.B[0][1],
             np.array([[0], [0], [0], [0.125], [-1.75], [6.875]]))
-        self.assertEqual(self.ce.input_function, self.u)
+        self.assertEqual(self.ce.input_functions, self.u)
 
     def test_simulate_state_space(self):
         """
@@ -1492,7 +1569,7 @@ class RadDirichletModalVsWeakFormulationTest(unittest.TestCase):
         a_mat = np.diag(eig_values)
         b_mat = -a2 * np.atleast_2d(
             [fraction(l) for fraction in adjoint_eig_base.derive(1).fractions]).T
-        ss_modal = sim.StateSpace(a_mat, b_mat, input_handle=u)
+        ss_modal = sim.StateSpace(a_mat, b_mat, input_handles=u)
 
         # check if ss_modal.(A,B) is close to ss_weak.(A,B)
         np.testing.assert_array_almost_equal(
@@ -1571,7 +1648,7 @@ class RadRobinModalVsWeakFormulationTest(unittest.TestCase):
         # determine pair (A, B) by modal transformation
         a_mat = np.diag(np.real_if_close(eig_val))
         b_mat = a2 * np.atleast_2d([fraction(l) for fraction in adjoint_eig_base.fractions]).T
-        ss_modal = sim.StateSpace(a_mat, b_mat, input_handle=u)
+        ss_modal = sim.StateSpace(a_mat, b_mat, input_handles=u)
 
         # check if ss_modal.(A,B) is close to ss_weak.(A,B)
         np.testing.assert_array_almost_equal(np.sort(np.linalg.eigvals(ss_weak.A[1])), np.sort(np.linalg.eigvals(ss_modal.A[1])),