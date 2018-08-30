a5b08ecfa7d6e86512383513210d378cf4449e7c
Marcus Riesmeier <marcus.riesmeier@umit.at>
2018-08-30 21:42:34 +0200

    Some progress with ocf weak formulation

--- pyinduct/examples/string_with_mass/main.py
+++ pyinduct/examples/string_with_mass/main.py
@@ -13,7 +13,7 @@ def main():
     z_end = 1
     spatial_discretization = 100
     spatial_domain = pi.Domain((0, z_end), spatial_discretization)
-    spat_domain_can = pi.Domain((-z_end, z_end), spatial_discretization)
+    spat_domain_cf = pi.Domain((-z_end, z_end), spatial_discretization)
     t_end = 10
     temporal_discretization = 300
     temporal_domain = pi.Domain((0, t_end), temporal_discretization)
@@ -29,9 +29,10 @@ def main():
     # set up bases
     sys_lbl = "fem_system"
     obs_lbl = "fem_observer"
-    fem_funcs1_nodes = pi.Domain(spatial_domain.bounds, 5)
-    fem_funcs2_nodes = pi.Domain(spatial_domain.bounds, 5)
-    build_fem_bases(sys_lbl, fem_funcs1_nodes, fem_funcs2_nodes)
+    n1 = 6
+    n2 = 6
+    ncf = 8
+    build_fem_bases(sys_lbl, n1, n2, obs_lbl, ncf)
 
     # controller
     controller = build_controller(sys_lbl)
@@ -41,29 +42,37 @@ def main():
     sys_wf = build_original_weak_formulation(
         sys_lbl, spatial_domain, input_, sys_lbl)
     obs_wf = build_canonical_weak_formulation(
-        obs_lbl, spat_domain_can, input_, obs_lbl)
+        obs_lbl, spat_domain_cf, input_, obs_lbl)
 
     # simulation
-    init_cond = {sys_wf.name: [SwmBaseFraction(
-        [pi.Function.from_constant(0), pi.Function.from_constant(0)], [0, 0])]}
-    spatial_domains = {sys_wf.name: spatial_domain}
+    init_cond = {
+        sys_wf.name: [SwmBaseFraction(
+        [pi.Function.from_constant(0), pi.Function.from_constant(0)], [0, 0])],
+        obs_wf.name: [SwmBaseCanonicalFraction(
+        [pi.Function.from_constant(0)], [0, 0])]}
+    spatial_domains = {sys_wf.name: spatial_domain, obs_wf.name: spat_domain_cf}
     ceq, ss, init_weights, weights, evald = pi.simulate_systems(
-        [sys_wf], init_cond, temporal_domain, spatial_domains,
+        [sys_wf, obs_wf], init_cond, temporal_domain, spatial_domains,
     )
 
     # check eigenvalues of the approximation
     pprint()
-    pprint("Eigenvalues of the approximation:")
-    pprint(sort_eigenvalues(np.linalg.eigvals(ss.A[1])))
+    pprint("Eigenvalues of the system:")
+    pprint(sort_eigenvalues(np.linalg.eigvals(ceq[0].dynamic_forms["fem_system"].e_n_pb_inv @
+                                              ceq[0].dynamic_forms["fem_system"].matrices["E"][0][1])))
+    pprint("Eigenvalues of the observer:")
+    pprint(sort_eigenvalues(np.linalg.eigvals(ceq[1].dynamic_forms["fem_observer"].e_n_pb_inv @
+                                              ceq[1].dynamic_forms["fem_observer"].matrices["E"][0][1])))
+
 
     # visualization
     eval_data1 = pi.get_sim_result(sys_lbl + "_1_visu", weights, temporal_domain, spatial_domain, 0, 0, name="x1(z,t)")
-    eval_data2 = pi.get_sim_result(sys_lbl + "_2_visu", weights, temporal_domain, spatial_domain, 0, 0, name="x2(z,t)")
+    # eval_data2 = pi.get_sim_result(sys_lbl + "_2_visu", weights, temporal_domain, spatial_domain, 0, 0, name="x2(z,t)")
     eval_data3 = pi.get_sim_result(sys_lbl + "_3_visu", weights, temporal_domain, spatial_domain, 0, 0, name="xi1(t)")
-    eval_data4 = pi.get_sim_result(sys_lbl + "_4_visu", weights, temporal_domain, spatial_domain, 0, 0, name="xi2(t)")
+    # eval_data4 = pi.get_sim_result(sys_lbl + "_4_visu", weights, temporal_domain, spatial_domain, 0, 0, name="xi2(t)")
     plots = list()
-    plots.append(pi.PgAnimatedPlot(eval_data2 + eval_data4))
     plots.append(pi.PgAnimatedPlot(eval_data1 + eval_data3))
+    # plots.append(pi.PgAnimatedPlot(eval_data2 + eval_data4))
     pi.show()
 
 
--- pyinduct/examples/string_with_mass/system.py
+++ pyinduct/examples/string_with_mass/system.py
@@ -1,36 +1,43 @@
 from pyinduct.examples.string_with_mass.utils import *
+import collections
 import pyinduct as pi
 from tqdm import tqdm
 
 
-def build_canonical_weak_formulation(sys_lbl, spatial_domain, input_, name="system"):
+def build_canonical_weak_formulation(obs_lbl, spatial_domain, input_, name="system"):
     r"""
-    The observer from [Woi2012] (equation 41)
+    Observer canonical form of the string with mass example
 
     .. math::
         :nowrap:
 
         \begin{align*}
-            \dot{\hat{\eta}}_1(t) &= \frac{2}{m}u(t) - (1+\alpha)k_0\tilde{y}(t) \\
-            \dot{\hat{\eta}}_2(t) &= \hat{\eta}_1(t) + \frac{2}{m}u(t)-((1+\alpha)k_1+2k_0)\tilde{y}(t) \\
-            \dot{\hat{\eta}}_3(\theta,t) &= -\hat{\eta}_3'(\theta,t)-\frac{2}{m}(1-h(\theta))\theta u(t)
-                - m^{-1} \hat{y}(t) \\
-            &\qquad -(k_0(1-\theta)+k_1-m^{-1})\tilde{y}(t)
+            \dot{x}_1(t) &= \frac{2}{m}u(t) \\
+            \dot{x}_2(t) &= x_1(t) + \frac{2}{m}u(t) \\
+            \dot{x}_3(z,t) &= -x_3'(z,t)-\frac{2}{m}(1-h(z))z u(t) - m^{-1} y(t)
         \end{align*}
 
-    .. math:: \hat{\eta}_3(-1,t) = \hat{\eta}_2(t) -\hat{y}(t)-(\alpha-1)\tilde{y}(t)
+    Boundary condition
 
-    is considered through integration by parts of the term
-    :math:`-\langle\hat{\eta}_3'(\theta),\psi_j(\theta)\rangle` from the weak formulation of equation 41a:
+    .. math:: x_3(-1,t) = x_2(t) - y(t)
+
+    Weak formulation
 
     .. math::
         :nowrap:
 
         \begin{align*}
-            -\langle\hat{\eta}_3'(\theta),\psi_j(\theta)\rangle &=
-            -\hat{\eta}_3(1)\psi_j'(1) + \hat{\eta}_3(-1)\psi_j'(-1)
-            +\langle\hat{\eta}_3(\theta),\psi_j'(\theta)\rangle.
+            -\langle \dot x(z, t), \psi(z)\rangle &=
+            \frac{2}{m}u(t)\psi_1 + \frac{2}{m}u(t)\psi_2 + x_1\psi_2
+            -x_3(1,t)\psi_3(1) - m^{-1}\langle y(t), \psi_3(z) \rangle \\
+            &+ \underbrace{x_3(-1,t)\psi_3(-1)}_{x_2(t)\psi_3(-1) - y(t)\psi_3(-1)}
+            + \langle x_3(z,t) , \psi_3'(z)\rangle
+            + \frac{2}{m}\langle (1 - h(z))z , \psi_3(z)\rangle u(t)
         \end{align*}
+
+    Output equation
+
+    .. math:: x_3(1,t) =  y(t)
 
     Args:
         sys_approx_label (string): Shapefunction label for system approximation.
@@ -44,66 +51,36 @@ def build_canonical_weak_formulation(sys_lbl, spatial_domain, input_, name="syst
     Returns:
         :py:class:`pyinduct.simulation.Observer`: Observer
     """
-    pass
-    # obs_err = pi.Controller(pi.WeakFormulation(
-    #     [
-    #         pi.ScalarTerm(pi.FieldVariable(sys_approx_label, location=0), scale=-1),
-    #         pi.ScalarTerm(eta3(-1).derive(spat_order=1), scale=-params.m / 2),
-    #         pi.ScalarTerm(eta3(1).derive(spat_order=1), scale=-params.m / 2),
-    #         pi.ScalarTerm(eta1(0), scale=-params.m / 2),
-    #     ],
-    #     name="nf observer error"))
-    # input_vector.append([obs_err])
-    #
-    # d_eta1 = pi.WeakFormulation(
-    #     [
-    #         pi.ScalarTerm(eta1(0).derive(temp_order=1), scale=-1),
-    #         pi.ScalarTerm(pi.Input(input_vector, index=0), scale=2 / params.m),
-    #         pi.ScalarTerm(pi.Input(input_vector, index=1),
-    #                       scale=0 if approx_bounded_part else -(1 + params.alpha_ob) * params.k0_ob)
-    #     ],
-    #     name=obs_approx_label[0]
-    # )
-    # d_eta2 = pi.WeakFormulation(
-    #     [
-    #         pi.ScalarTerm(eta2(0).derive(temp_order=1), scale=-1),
-    #         # index error in paper
-    #         pi.ScalarTerm(eta1(0)),
-    #         pi.ScalarTerm(pi.Input(input_vector, index=0), scale=2 / params.m),
-    #         pi.ScalarTerm(pi.Input(input_vector, index=1),
-    #                       scale=0 if approx_bounded_part else -(1 + params.alpha_ob) * params.k1_ob - 2 * params.k0_ob)
-    #     ],
-    #     name=obs_approx_label[1]
-    # )
-    # d_eta3 = pi.WeakFormulation(
-    #     [
-    #         pi.IntegralTerm(pi.Product(eta3.derive(temp_order=1), psi), limits=limits, scale=-1),
-    #         # sign error in paper
-    #         pi.IntegralTerm(pi.Product(pi.Product(obs_scale1, psi), pi.Input(input_vector, index=0)), limits=limits,
-    #                         scale=-1),
-    #         pi.IntegralTerm(pi.Product(pi.Product(obs_scale2, psi), pi.Input(input_vector, index=1)), limits=limits,
-    #                         scale=0 if approx_bounded_part else 1),
-    #         # \hat y
-    #         pi.IntegralTerm(pi.Product(eta3(-1).derive(spat_order=1), psi), limits=limits, scale=1 / 2),
-    #         pi.IntegralTerm(pi.Product(eta3(1).derive(spat_order=1), psi), limits=limits, scale=1 / 2),
-    #         pi.IntegralTerm(pi.Product(eta1, psi), limits=limits, scale=1 / 2),
-    #         # shift
-    #         pi.IntegralTerm(pi.Product(eta3, psi.derive(1)), limits=limits),
-    #         pi.ScalarTerm(pi.Product(eta3(1), psi(1)), scale=-1),
-    #         # bc
-    #         pi.ScalarTerm(pi.Product(psi(-1), eta2(0))),
-    #         pi.ScalarTerm(pi.Product(pi.Input(input_vector, index=1), psi(-1)), scale=0 if approx_unbounded_part else 1 - params.alpha_ob),
-    #         # bc \hat y
-    #         pi.ScalarTerm(pi.Product(eta3(-1).derive(spat_order=1), psi(-1)), params.m / 2),
-    #         pi.ScalarTerm(pi.Product(eta3(1).derive(spat_order=1), psi(-1)), params.m / 2),
-    #         pi.ScalarTerm(pi.Product(psi(-1), eta1(1)), params.m / 2),
-    #         # approximate unbouned part
-    #         # pi.ScalarTerm(pi.Product(pi.Input(input_vector, index=1), psi(1)), scale=1-params.alpha_ob if approx_unbounded_part else 0),
-    #     ],
-    #     name=obs_approx_label[2]
-    # )
-    #
-    # return [d_eta1, d_eta2, d_eta3]
+    x = pi.FieldVariable(obs_lbl)
+    psi = pi.TestFunction(obs_lbl)
+    psi1 = pi.TestFunction(obs_lbl + "_10")
+    psi2 = pi.TestFunction(obs_lbl + "_20")
+    psi3 = pi.TestFunction(obs_lbl + "_30")
+    psi2_x1 = pi.TestFunction(obs_lbl + "_21")
+    psi3_x2_at_m1 = pi.TestFunction(obs_lbl + "_32_at_m1")
+    psi3_integrated = pi.TestFunction(obs_lbl + "_3_integrated")
+    psi_3_int_intup_scale_f = pi.TestFunction(obs_lbl + "_3_integred_with_input_scale")
+
+    dummy = 0
+    u = pi.Input(input_)
+    bounds = spatial_domain.bounds
+    wf = pi.WeakFormulation(
+        [
+            pi.IntegralTerm(pi.Product(x.derive(temp_order=1), psi), limits=bounds, scale=-1),
+            pi.ScalarTerm(pi.Product(u, psi1(dummy)), scale=2 / param.m),
+            pi.ScalarTerm(pi.Product(u, psi2(dummy)), scale=2 / param.m),
+            pi.ScalarTerm(pi.Product(x(dummy), psi2_x1(dummy))),
+            pi.ScalarTerm(pi.Product(x(1), psi3(1)), scale=-1),
+            pi.ScalarTerm(pi.Product(x(dummy), psi3_x2_at_m1(dummy))),
+            pi.ScalarTerm(pi.Product(x(1), psi3(-1)), scale=-1),
+            pi.IntegralTerm(pi.Product(x, psi3.derive(1)), limits=bounds),
+            pi.ScalarTerm(pi.Product(u, psi_3_int_intup_scale_f(dummy)), scale=-1),
+            pi.ScalarTerm(pi.Product(x(1), psi3_integrated(dummy)), scale=-param.m ** -1)
+        ],
+        name=name
+    )
+
+    return wf
 
 
 def build_original_weak_formulation(sys_lbl, spatial_domain, input_, name="system"):
@@ -176,10 +153,14 @@ def build_original_weak_formulation(sys_lbl, spatial_domain, input_, name="syste
     return wf
 
 
-def build_fem_bases(base_lbl, nodes1, nodes2):
+def build_fem_bases(base_lbl, n1, n2, cf_base_lbl, ncf):
+    nodes1 = pi.Domain((0, 1), n1)
+    nodes2 = pi.Domain((0, 1), n2)
+    cf_nodes = pi.Domain((-1, 1), ncf)
     assert nodes1.bounds == nodes2.bounds
-    fem_funcs1 = pi.LagrangeNthOrder.cure_interval(nodes1, order=4)
-    fem_funcs2 = pi.LagrangeNthOrder.cure_interval(nodes2, order=4)
+
+    fem_funcs1 = pi.LagrangeNthOrder.cure_interval(nodes1, order=1)
+    fem_funcs2 = pi.LagrangeNthOrder.cure_interval(nodes2, order=1)
     zero_function = pi.Function.from_constant(0, domain=nodes1.bounds)
     one_function = pi.Function.from_constant(1, domain=nodes1.bounds)
 
@@ -205,7 +186,10 @@ def build_fem_bases(base_lbl, nodes1, nodes2):
     base4_x1 = [SwmBaseFraction([one_function, zero_function], [0, 0])]
 
     # bases for the system / weak formulation
-    pi.register_base(base_lbl, pi.Base(base1 + base2 + base4))
+    pi.register_base(base_lbl, pi.Base(
+        base1 + base2 + base4, associated_bases=[
+            base_lbl + st for st in ("_1_visu", "_2_visu", "_3_visu", "_4_visu")
+        ]))
     pi.register_base(base_lbl + "_12", pi.Base(base12 + base20 + base40))
     pi.register_base(base_lbl + "_21", pi.Base(base10 + base21 + base40))
     pi.register_base(base_lbl + "_1_xi2_at_0", pi.Base(base14_at_0 + base20 + base40))
@@ -224,6 +208,47 @@ def build_fem_bases(base_lbl, nodes1, nodes2):
     pi.register_base(base_lbl + "_2_visu", pi.Base(zb1 + fb2 + zb4, matching_bases=[base_lbl]))
     pi.register_base(base_lbl + "_3_visu", pi.Base(ob1 + zb2 + zb4, matching_bases=[base_lbl]))
     pi.register_base(base_lbl + "_4_visu", pi.Base(zb1 + zb2 + ob4, matching_bases=[base_lbl]))
+
+    def heavi(z):
+        return 0 if z < 0 else (0.5 if z == 0 else 1)
+
+    # bases for the canonical form
+    cf_fem_funcs = pi.LagrangeNthOrder.cure_interval(cf_nodes, order=1)
+    cf_zero_func = pi.Function.from_constant(0, domain=cf_nodes.bounds)
+    cf_one_func = pi.Function.from_constant(1, domain=cf_nodes.bounds)
+    input_scale = lambda z: -2 / param.m * (heavi(z) - 1) * z
+
+    cf_base1 = [SwmBaseCanonicalFraction([cf_zero_func], [1, 0])]
+    cf_base10 = [SwmBaseCanonicalFraction([cf_zero_func], [0, 0])]
+
+    cf_base2 = [SwmBaseCanonicalFraction([cf_zero_func], [0, 1])]
+    cf_base20 = [SwmBaseCanonicalFraction([cf_zero_func], [0, 0])]
+    cf_base21 = [SwmBaseCanonicalFraction([cf_zero_func], [1, 0])]
+
+    from pyinduct.core import integrate_function
+    cf_base3, cf_base30, cf_base32_at_m1, cf_base3_integrated, cf_base3_int_ip_scale = [
+        list() for _ in range(5)]
+    for f in cf_fem_funcs:
+        cf_base3.append(SwmBaseCanonicalFraction([f], [0, 0]))
+        cf_base30.append(SwmBaseCanonicalFraction([cf_zero_func], [0, 0]))
+        cf_base32_at_m1.append(SwmBaseCanonicalFraction([cf_zero_func], [0, f(-1)]))
+        cf_base3_integrated.append(SwmBaseCanonicalFraction([pi.Function.from_constant(
+            float(integrate_function(f, [(-1, 1)])[0]), domain=cf_nodes.bounds)], [0, 0]))
+        cf_base3_int_ip_scale.append(SwmBaseCanonicalFraction([pi.Function.from_constant(
+            float(integrate_function(lambda z: f(z) * input_scale(z), [(-1, 1)])[0]),
+            domain=cf_nodes.bounds)], [0, 0]))
+
+    # bases for the system / weak formulation
+    pi.register_base(cf_base_lbl, pi.Base(cf_base1 + cf_base2 + cf_base3))
+    pi.register_base(cf_base_lbl + "_10", pi.Base(cf_base1 + cf_base20 + cf_base30))
+    pi.register_base(cf_base_lbl + "_20", pi.Base(cf_base10 + cf_base2 + cf_base30))
+    pi.register_base(cf_base_lbl + "_30", pi.Base(cf_base10 + cf_base20 + cf_base3))
+    pi.register_base(cf_base_lbl + "_21", pi.Base(cf_base10 + cf_base21 + cf_base3))
+    pi.register_base(cf_base_lbl + "_32_at_m1", pi.Base(cf_base10 + cf_base20 + cf_base32_at_m1))
+    pi.register_base(cf_base_lbl + "_3_integrated",
+                     pi.Base(cf_base10 + cf_base20 + cf_base3_integrated))
+    pi.register_base(cf_base_lbl + "_3_integred_with_input_scale",
+                     pi.Base(cf_base10 + cf_base20 + cf_base3_int_ip_scale))
 
 
 def register_evp_base(base_lbl, eigenvectors, sp_var, domain):
@@ -365,7 +390,14 @@ class SwmBaseCanonicalFraction(pi.ComposedFunctionVector):
                         [f(z) for f in self.members["funcs"]])
 
     def evaluation_hint(self, values):
-        return self(values)[0]
+        if isinstance(values, (collections.Iterable, pi.Domain)):
+            res = list()
+            for val in values:
+                res.append(self(val))
+            return np.array(res)[:, 2]
+
+        else:
+            return self(values)[2]
 
     def derive(self, order):
         if order == 0:
